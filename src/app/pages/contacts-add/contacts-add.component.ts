df304030c863c13524967d107547f7dea2051e4e
Fernanda Dutra <fernandanunesdutra96@gmail.com>
2020-06-12 17:34:42 -0300

    Pagina de contato

---
+++ src/app/pages/contacts-add/contacts-add.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-actions-add',
+  templateUrl: './actions-add.component.html',
+  styleUrls: ['./actions-add.component.scss']
+})
+export class ActionsAddComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
